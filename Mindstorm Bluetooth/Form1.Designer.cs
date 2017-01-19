f5a49b4132cd80edcbad204dd20595bf70d13f95
danielvaijk <danielvaijk@gmail.com>
2017-01-20 00:56:36 +0100

    First commit

---
+++ Mindstorm Bluetooth/Form1.Designer.cs
@@ -0,0 +1,283 @@
+﻿namespace Mindstorm_Bluetooth
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.statusLabel = new System.Windows.Forms.Label();
+            this.button9 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button10 = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(7, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(71, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "COM PORTS";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(235, 14);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(141, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Connect";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button5
+            // 
+            this.button5.Enabled = false;
+            this.button5.Location = new System.Drawing.Point(235, 43);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(141, 23);
+            this.button5.TabIndex = 10;
+            this.button5.Text = "Disconnect";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.comboBox1);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.button5);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(382, 76);
+            this.groupBox1.TabIndex = 11;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Connection Options";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.CausesValidation = false;
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.ImeMode = System.Windows.Forms.ImeMode.Off;
+            this.comboBox1.Location = new System.Drawing.Point(84, 16);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(141, 21);
+            this.comboBox1.TabIndex = 23;
+            // 
+            // statusLabel
+            // 
+            this.statusLabel.AutoSize = true;
+            this.statusLabel.Location = new System.Drawing.Point(9, 369);
+            this.statusLabel.Name = "statusLabel";
+            this.statusLabel.Size = new System.Drawing.Size(109, 13);
+            this.statusLabel.TabIndex = 14;
+            this.statusLabel.Text = "Status: Disconnected";
+            // 
+            // button9
+            // 
+            this.button9.Location = new System.Drawing.Point(6, 26);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(153, 37);
+            this.button9.TabIndex = 17;
+            this.button9.Text = "START";
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(7, 39);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(42, 13);
+            this.label4.TabIndex = 18;
+            this.label4.Text = "Label 4";
+            // 
+            // button10
+            // 
+            this.button10.Enabled = false;
+            this.button10.Location = new System.Drawing.Point(6, 69);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(153, 37);
+            this.button10.TabIndex = 19;
+            this.button10.Text = "STOP";
+            this.button10.UseVisualStyleBackColor = true;
+            this.button10.Click += new System.EventHandler(this.button10_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(7, 52);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(42, 13);
+            this.label5.TabIndex = 20;
+            this.label5.Text = "Label 4";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(7, 26);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(42, 13);
+            this.label6.TabIndex = 21;
+            this.label6.Text = "Label 4";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(7, 65);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(42, 13);
+            this.label7.TabIndex = 22;
+            this.label7.Text = "Label 4";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.textBox2);
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Controls.Add(this.textBox1);
+            this.groupBox2.Controls.Add(this.label2);
+            this.groupBox2.Controls.Add(this.button9);
+            this.groupBox2.Controls.Add(this.button10);
+            this.groupBox2.Location = new System.Drawing.Point(12, 95);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(382, 112);
+            this.groupBox2.TabIndex = 23;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Control Panel";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(259, 47);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(117, 20);
+            this.textBox2.TabIndex = 23;
+            this.textBox2.Text = "10";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(187, 50);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(64, 13);
+            this.label3.TabIndex = 22;
+            this.label3.Text = "Grab Speed";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(259, 23);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(117, 20);
+            this.textBox1.TabIndex = 21;
+            this.textBox1.Text = "2";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(187, 26);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(66, 13);
+            this.label2.TabIndex = 20;
+            this.label2.Text = "Drive Speed";
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.label6);
+            this.groupBox3.Controls.Add(this.label4);
+            this.groupBox3.Controls.Add(this.label7);
+            this.groupBox3.Controls.Add(this.label5);
+            this.groupBox3.Location = new System.Drawing.Point(6, 249);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(382, 88);
+            this.groupBox3.TabIndex = 24;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Status Buffer";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(406, 391);
+            this.ControlBox = false;
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.statusLabel);
+            this.Controls.Add(this.groupBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Name = "Form1";
+            this.Text = "Mindstorm Bluetooth Manager";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label statusLabel;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button10;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ Mindstorm Bluetooth/Form1.cs
@@ -0,0 +1,399 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Timers;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO.Ports;
+using System.Management;
+
+using Lego.Ev3.Core;
+using Lego.Ev3.Desktop;
+
+// Written by Daniel van Dijk. Last modified: 12-09-16
+
+/* NOTES 
+ 
+     1. [ADD] reference System.IO.Ports if you want to have a list of COM Ports appear for the user to choose from. 
+     2. Port A has to be the left motor and port B has to be the right motor.
+     3. [ADD] Grabber angle guessing
+     4. [ADD] Sensor code
+     5. [ADD] Motor movement only while button is held down
+     6. [ADD] Don't perform any functions on the grabber while it is either opening or closing */
+
+namespace Mindstorm_Bluetooth
+{
+    public partial class Form1 : Form
+    {
+        private bool isConnected = false;
+        private bool started = false;
+        private bool ranCommand = false;
+        //private bool obstructed = false;
+
+        private Brick brick;
+
+        private int color = 0;
+        private int polarity = 0;
+
+        private float distance = 0;
+        private double turnAmount = 3;
+
+        private string lastColor;
+
+        private Stopwatch watch = new Stopwatch();
+
+        private Status currentStatus = Status.Waiting;
+
+        // Represents the current status of the AGV.
+        public enum Status
+        {
+            Waiting,
+            PathBlocked,
+            FindingPolarity,
+            FindingPath,
+            MovingToPickup,
+            Grabbing,
+            MovingToDropoff,
+            Dropping,
+            MovingToEnd
+        }
+
+        // Dictionary with all the text equivalents of each status.
+        Dictionary<Status, string> statusValues = new Dictionary<Status, string>();
+
+        public Form1 ()
+        {
+            InitializeComponent();
+        }
+
+        // Called when <Form1> is fully loaded.
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // Set the text value equivalents to each status.
+            statusValues.Add(Status.Waiting, "Waiting");
+            statusValues.Add(Status.PathBlocked, "Path is blocked");
+            statusValues.Add(Status.FindingPolarity, "Finding polarity");
+            statusValues.Add(Status.FindingPath, "Finding path");
+            statusValues.Add(Status.MovingToPickup, "Moving to pickup area");
+            statusValues.Add(Status.Grabbing, "Grabbing object");
+            statusValues.Add(Status.MovingToDropoff, "Moving to dropoff area");
+            statusValues.Add(Status.Dropping, "Dropping object");
+            statusValues.Add(Status.MovingToEnd, "Moving to end point");
+
+            // Add currently used SerialPorts for selection.
+            foreach (string port in SerialPort.GetPortNames())
+                comboBox1.Items.Add(port);
+
+            // Default to the first item, if any.
+            if (comboBox1.Items.Count > 0)
+                comboBox1.SelectedIndex = 0;
+        }
+
+        // Connect button.
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (comboBox1.Items.Count > 0 && comboBox1.SelectedItem != null)
+            {
+                statusLabel.Text = "Status: Connecting";
+
+                // Establish a new bluetooth connection based on the port <port>.
+                ICommunication com = new BluetoothCommunication(comboBox1.SelectedItem.ToString());
+
+                com.ReportReceived += Com_ReportReceived;
+
+                // Setup the connection timer so we can handle connection timeouts.
+                System.Timers.Timer connectionTimer = new System.Timers.Timer(1000 * 10);
+
+                connectionTimer.Elapsed += ConnectionTimer_Elapsed;
+                connectionTimer.Enabled = true;
+
+                // Set and connect the brick.
+                brick = new Brick(com);
+                brick.ConnectAsync();
+
+                brick.BrickChanged += Brick_BrickChanged;
+            }
+        }
+
+        private async void Brick_BrickChanged(object sender, BrickChangedEventArgs e)
+        {
+            color = e.Ports[InputPort.Two].RawValue;
+            distance = e.Ports[InputPort.Three].SIValue;
+
+            label4.Text = string.Format("RAW {0} | SI {1}", e.Ports[InputPort.Two].RawValue, e.Ports[InputPort.Two].SIValue);
+            label5.Text = string.Format("RAW {0} | SI {1}", e.Ports[InputPort.Three].RawValue, e.Ports[InputPort.Three].SIValue);
+            label6.Text = string.Format("Polarity {0} Turn Vector {1}", polarity, turnAmount);
+
+            label7.Text = "Roboto Status: " + statusValues[currentStatus];
+            //label7.Text = "DISTANCE: " + distance;
+
+            if (polarity == 0 && started)
+            {
+                if (color == 4 || color == 5)
+                {
+                    // BUG WARNING
+                    polarity = (color == 4) ? 1 : -1;
+                    currentStatus = Status.MovingToPickup;
+                }
+            }
+
+            //button9.Enabled = !(obstructed = distance < 6);
+
+            /* if (obstructed && (color == 1 || color == 7))
+             {
+                 turnAmount = 5;
+                 await brick.DirectCommand.StopMotorAsync(OutputPort.B | OutputPort.D, false);
+                 currentStatus = Status.PathBlocked;
+                 return;
+             }*/
+
+            button9.Enabled = (color == 1 || color == 7);
+
+            int speed = GetSpeed(textBox1.Text) * (int)Math.Floor(turnAmount);
+
+            if (currentStatus == Status.Grabbing)
+            {
+                if (!watch.IsRunning && !ranCommand)
+                    watch.Start();
+
+                if (!ranCommand && distance < 5)
+                {
+                    ranCommand = true;
+
+                    // Stop the timer
+                    watch.Stop();
+
+                    // Stop the motors
+                    await brick.DirectCommand.StopMotorAsync(OutputPort.B | OutputPort.D, true);
+
+                    // Close claw (grab object)
+                    await brick.DirectCommand.TurnMotorAtSpeedAsync(OutputPort.C, GetSpeed(textBox2.Text));
+                    await Task.Delay(TimeSpan.FromSeconds(0.5));
+                    await brick.DirectCommand.StopMotorAsync(OutputPort.C, true);
+
+                    // Go backwards by (time in white) x speed
+                    await brick.DirectCommand.TurnMotorAtSpeedAsync(OutputPort.B | OutputPort.D, -GetSpeed(textBox1.Text));
+                    await Task.Delay(TimeSpan.FromMilliseconds(watch.ElapsedMilliseconds));
+                    await brick.DirectCommand.StopMotorAsync(OutputPort.B | OutputPort.D, true);
+
+                    // Turn towards the next entry point for the path.
+                    await brick.DirectCommand.TurnMotorAtSpeedAsync(OutputPort.B, GetSpeed(textBox1.Text) * polarity + 5);
+
+                    await Task.Delay(TimeSpan.FromSeconds(4));
+
+                    await brick.DirectCommand.StopMotorAsync(OutputPort.B, true);
+                    await brick.DirectCommand.TurnMotorAtSpeedAsync(OutputPort.B | OutputPort.D, GetSpeed(textBox1.Text));
+
+                    currentStatus = Status.FindingPath;
+                    return;
+                }
+            }
+            else if (currentStatus == Status.Dropping)
+            {
+                await brick.DirectCommand.StopMotorAsync(OutputPort.B | OutputPort.D, true);
+            }
+
+            switch (color)
+            {
+                case 1:
+                case 7:
+                    button9.Text = "Black";
+
+                    if (polarity == 0)
+                        return;
+
+                    if (currentStatus == Status.FindingPath)
+                    {
+                        currentStatus = Status.MovingToDropoff;
+                        /*if (currentStatus == Status.Grabbing)
+                            currentStatus = Status.MovingToDropoff;
+                        else if (currentStatus == Status.Dropping)
+                            currentStatus = Status.MovingToEnd;*/
+                    }
+
+                    turnAmount = 3;
+
+                    // Go straight.
+                    await brick.DirectCommand.TurnMotorAtSpeedAsync(OutputPort.D, GetSpeed(textBox1.Text));
+                    await brick.DirectCommand.TurnMotorAtSpeedAsync(OutputPort.B, GetSpeed(textBox1.Text));
+                    break;
+
+                case 4:
+                    button9.Text = "Yellow";
+
+                    if (polarity == 0 || currentStatus == Status.FindingPath)
+                        return;
+
+                    if (lastColor != "Yellow")
+                    {
+                        await brick.DirectCommand.TurnMotorAtSpeedAsync(OutputPort.D, GetSpeed(textBox1.Text));
+                        await brick.DirectCommand.TurnMotorAtSpeedAsync(OutputPort.B, GetSpeed(textBox1.Text));
+                        lastColor = "Yellow";
+                    }
+
+                    turnAmount += 0.6;
+
+                    if (polarity == 1)
+                        await brick.DirectCommand.TurnMotorAtSpeedAsync(OutputPort.D, -speed);
+                    else
+                        await brick.DirectCommand.TurnMotorAtSpeedAsync(OutputPort.B, -speed);
+
+                    break;
+
+                case 5:
+                    button9.Text = "Red";
+
+                    if (polarity == 0 || currentStatus == Status.FindingPath)
+                        return;
+
+                    if (lastColor != "Red")
+                    {
+                        await brick.DirectCommand.TurnMotorAtSpeedAsync(OutputPort.D, GetSpeed(textBox1.Text));
+                        await brick.DirectCommand.TurnMotorAtSpeedAsync(OutputPort.B, GetSpeed(textBox1.Text));
+                        lastColor = "Red";
+                    }
+
+                    turnAmount += 0.6;
+
+                    if (polarity == 1)
+                        await brick.DirectCommand.TurnMotorAtSpeedAsync(OutputPort.B, -speed);
+                    else
+                        await brick.DirectCommand.TurnMotorAtSpeedAsync(OutputPort.D, -speed);
+
+                    break;
+
+                case 6:
+                    button9.Text = "White";
+
+                    if (polarity == 0)
+                        return;
+
+                    if (turnAmount > 9)
+                        turnAmount = 3;
+
+                    if (currentStatus == Status.MovingToPickup)
+                        currentStatus = Status.Grabbing;
+
+                    if (currentStatus == Status.MovingToDropoff)
+                        currentStatus = Status.Dropping;
+
+                    break;
+            }
+        }
+
+        // Called when the <connectionTimer> has elapsed.
+        private void ConnectionTimer_Elapsed (object sender, ElapsedEventArgs e)
+        {
+            MethodInvoker mi = delegate ()
+            {
+                // Invoke this on the main thread.
+                statusLabel.Text = "Status: Connection timed out";
+            };
+
+            Invoke(mi);
+        }
+
+        // Called when a full report is received from the brick, confirming a succesfull connection.
+        private void Com_ReportReceived(object sender, ReportReceivedEventArgs e)
+        {
+            MethodInvoker mi = delegate ()
+            {
+                // Invoke this on the main thread.
+                isConnected = true;
+                statusLabel.Text = "Status: Connected";
+
+                // Set the modes for each sensor in the sensor ports.
+                brick.Ports[InputPort.Two].SetMode(ColorMode.Color);
+                brick.Ports[InputPort.Three].SetMode(UltrasonicMode.Centimeters);
+
+                // Change form controls based on new connection status.
+                button1.Enabled = false; // Connect
+                comboBox1.Enabled = false; // Ports List
+                button5.Enabled = true; // Disconnect
+            };
+
+            Invoke(mi);
+        }
+
+        // Called when the user has released the mouse button from a button. Will stop motors connected to ports A and B.
+        private async void StopMotors (object sender, MouseEventArgs e)
+        {
+            await brick.DirectCommand.StopMotorAsync(OutputPort.A | OutputPort.B, false);
+        }
+
+        // Disconnect button.
+        private void button5_Click(object sender, EventArgs e)
+        {
+            // Disconnect the brick.
+            brick.Disconnect();
+
+            // Clean up after disconnection.
+            statusLabel.Text = "Status: Disconnected";
+            isConnected = false;
+
+            // Change form controls based on new connection status.
+            button1.Enabled = true; // Connect
+            comboBox1.Enabled = true; // Ports List
+            button9.Enabled = false; // Start
+            button5.Enabled = false; // Disconnect
+
+            // Clean up the variables.
+            CleanUp();
+        }
+
+        // Returns the speed value based on the <textBox2.Text> string and deals accordingly with
+        // invalid parsing (string -> int) exceptions.
+        private int GetSpeed (string value)
+        {
+            int speed = 0;
+
+            if (!int.TryParse(value, out speed))
+            {
+                statusLabel.Text = "Status: Invalid speed value";
+            }
+            else if (speed < 0)
+            {
+                statusLabel.Text = "Status: Invalid speed (value < 0)";
+                speed = 0;
+            }
+            else if (speed == 0)
+            {
+                statusLabel.Text = "Status: Motor won't move (speed = 0)";
+            }
+
+            return speed;
+        }
+
+        private async void button9_Click(object sender, EventArgs e)
+        {
+            if (isConnected)
+            {
+                started = true;
+
+                currentStatus = Status.FindingPolarity;
+
+                //await brick.DirectCommand.TurnMotorAtSpeedAsync(OutputPort.B, -2);
+                await brick.DirectCommand.TurnMotorAtSpeedAsync(OutputPort.D, 1);
+
+                button10.Enabled = true; // Stop button
+            }
+
+            //await brick.DirectCommand.PlaySoundAsync(100, @"Project\THEME");
+        }
+
+        // Stop button
+        private async void button10_Click(object sender, EventArgs e)
+        {
+            await brick.DirectCommand.StopMotorAsync(OutputPort.B | OutputPort.D, true);
+            CleanUp();
+        }
+
+        private void CleanUp ()
+        {
+            started = false;
+            polarity = 0;
+            currentStatus = Status.Waiting;
+        }
+    }
+}
